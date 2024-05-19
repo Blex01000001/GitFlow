fb8874728d2ae300c2c1ff3864db51c10dee5f40
周政輝 <leo604leo604@gmail.com>
2024-05-19 18:17:23 +0800

    完成註冊功能實作

--- Models/User.cs
+++ Models/User.cs
@@ -2,6 +2,9 @@ namespace GitFlow.Models
 {
     public class User
     {
-        
+        public string Name { get; set; }
+        public string Account { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
     }
 }
